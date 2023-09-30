054d733e3450c52ccdb65f87a406d862818b8ccb
Touqeer Arshad <99006844+981488@users.noreply.github.com>
2023-09-30 10:24:47 -0700

    Add files via upload

---
+++ Exercise2/Exercise2.js
@@ -0,0 +1,4 @@
+"use strict";
+let personName = "Touqeer";
+let message = `Hello ${personName} , hope you are doing well these days`;
+console.log(message);
---
+++ Exercise2/Exercise2.ts
@@ -0,0 +1,3 @@
+let personName: string = "Touqeer"
+let message: string = `Hello ${personName} , hope you are doing well these days`
+console.log(message)
