c4d36efe3a43a23b87423c31f133f4274b747cf6
Touqeer Arshad <99006844+981488@users.noreply.github.com>
2023-09-30 10:31:23 -0700

    Add files via upload

---
+++ Exercise4/index.ts
@@ -0,0 +1,3 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your out put should look Albert Einstein once said, "A person who never made a mistake never tried anything new."
+
+console.log(`Brokish Ray once said,"If hard work pays,show me a rich Donkey"`)
