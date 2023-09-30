a7ebb31705b2ae2198d5c0352d7d5598f5348515
Touqeer Arshad <99006844+981488@users.noreply.github.com>
2023-09-30 10:35:53 -0700

    Add files via upload

---
+++ Exercise5/Exercise5.ts
@@ -0,0 +1,5 @@
+// Famous Quote 2: Repeat Exercise 4,but this time store the famous person's name in variable called famous_person.Then compose your message and store it in a new variable called message.Print your message.
+
+let famous_person: string = "Brokish Ray";
+let message: string = famous_person + ` once said,"If hardwork pays,show me a rich Donkey."`
+console.log(message);
