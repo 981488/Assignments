b75a50f7e237ca8803262c3aaea79dc183201f9c
Touqeer Arshad <99006844+981488@users.noreply.github.com>
2023-10-03 09:20:28 -0700

    Add files via upload

---
+++ Exercise11/index.ts
@@ -0,0 +1,7 @@
+// Names: Store the names of a few friends in a array called names. Print each person's name by accessing each element in the list, one at a time.
+
+let names: string[] = ['Shakeel', 'Ali', 'Usman', 'Arjasab'];
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
