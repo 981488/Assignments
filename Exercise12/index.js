29978a9fc4250a9a68ab6d86c64274e0870b7469
Touqeer Arshad <99006844+981488@users.noreply.github.com>
2023-10-03 09:22:56 -0700

    Add files via upload

---
+++ Exercise12/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Greetings: Start with the array you used in exercise 11, but instead of just printing each person's name, print a message to them. The text of each message shoud be the same,
+// but each message shoud be personlaized with the person's name.
+let names = ['Shakeel', 'Ali', 'Usman', 'Arjasab'];
+// Using for loop through each name in the array 
+for (let name of names) {
+    // here we print a personalize message for each friend 
+    console.log(`Good morning ${name}, i hope you are doing well these days!`);
+}
