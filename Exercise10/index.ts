ba17041fe1945ca2e7b86dbd745dfdff8ab602d5
Touqeer Arshad <99006844+981488@users.noreply.github.com>
2023-10-03 09:18:01 -0700

    Add files via upload

---
+++ Exercise10/index.ts
@@ -0,0 +1,16 @@
+// Favorite Number: Store your favorite nember in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+//  if i want to comment line # 4 i will used Ctrl + /
+let favoriteNumber: number = 11
+let message: string = `My favorite number is ${favoriteNumber}`
+console.log(message)
+
+
+// if i want to commment all line of program i will used /*     */
+/*let favoriteNumber: number = 11
+let message: string = `My favorite number is ${favoriteNumber}`
+console.log(message)*/
+
+
+
+
+
